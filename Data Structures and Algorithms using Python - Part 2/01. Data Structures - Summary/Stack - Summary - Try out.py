a0b1a4ff5e6c327de92dea461ec278e7f974cfe4
ASH <25776660+iash8090@users.noreply.github.com>
2023-03-11 14:30:43 +0530

    Create Stack - Summary - Try out.py

---
+++ Data Structures and Algorithms using Python - Part 2/01. Data Structures - Summary/Stack - Summary - Try out.py
@@ -0,0 +1,132 @@
+class Stack:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__top=-1
+
+    def is_full(self):
+        if(self.__top==self.__max_size-1):
+            return True
+        return False
+
+    def is_empty(self):
+        if(self.__top==-1):
+            return True
+        return False
+
+    def push(self,data):
+        if(self.is_full()):
+            print("The stack is full!!")
+        else:
+            self.__top+=1
+            self.__elements[self.__top]=data
+
+    def pop(self):
+        if(self.is_empty()):
+            print("The stack is empty!!")
+        else:
+            data= self.__elements[self.__top]
+            self.__top-=1
+            return data
+
+    def display(self):
+        if(self.is_empty()):
+            print("The stack is empty")
+        else:
+            index=self.__top
+            while(index>=0):
+                print(self.__elements[index])
+                index-=1
+
+    def get_max_size(self):
+        return self.__max_size 
+    
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        msg=[]
+        index=self.__top
+        while(index>=0):
+            msg.append((str)(self.__elements[index]))
+            index-=1
+        msg=" ".join(msg)
+        msg="Stack data(Top to Bottom): "+msg
+        return msg    
+
+class Queue:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__rear=-1
+        self.__front=0
+
+    def is_full(self):
+        if(self.__rear==self.__max_size-1):
+            return True
+        return False
+
+    def is_empty(self):
+        if(self.__front>self.__rear):
+            return True
+        return False
+
+    def enqueue(self,data):
+        if(self.is_full()):
+            print("Queue is full!!!")
+        else:
+            self.__rear+=1
+            self.__elements[self.__rear]=data
+
+    def dequeue(self):
+        if(self.is_empty()):
+            print("Queue is empty!!!")
+        else:
+            data=self.__elements[self.__front]
+            self.__front+=1
+            return data
+
+    def display(self):
+        for index in range(self.__front, self.__rear+1):
+            print(self.__elements[index])
+
+    def get_max_size(self):
+        return self.__max_size
+                                        
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        msg=[]
+        index=self.__front
+        while(index<=self.__rear):
+            msg.append((str)(self.__elements[index]))
+            index+=1
+        msg=" ".join(msg)
+        msg="Queue data(Front to Rear): "+msg
+        return msg
+
+
+def fun(input_stack):
+    num=input_stack.get_max_size()-1
+    num1=1
+    while(num>0):
+        top_element=input_stack.pop()
+        temp_stack=Stack(input_stack.get_max_size())
+        num2=1
+        while(num2<=num1):
+            element=input_stack.pop()
+            temp_stack.push(element+top_element)
+            num2+=1
+        while(not temp_stack.is_empty()):
+            input_stack.push(temp_stack.pop())
+        input_stack.push(top_element)
+        num1+=1
+        num-=1
+    return input_stack
+
+sample= Stack(5)
+sample.push(8)
+sample.push(2)
+sample.push(6)
+sample.push(7)
+sample.push(10)
+result_stack=fun(sample)
+result_stack.display()
+
