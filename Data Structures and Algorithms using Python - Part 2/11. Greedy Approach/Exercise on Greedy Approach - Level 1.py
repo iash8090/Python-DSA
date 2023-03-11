6e3680e13e093f63ce666afbc3f4cdf147e83d57
ASH <25776660+iash8090@users.noreply.github.com>
2023-03-11 17:35:12 +0530

    Update Exercise on Greedy Approach - Level 1.py

--- Data Structures and Algorithms using Python - Part 2/11. Greedy Approach/Exercise on Greedy Approach - Level 1.py
+++ Data Structures and Algorithms using Python - Part 2/11. Greedy Approach/Exercise on Greedy Approach - Level 1.py
@@ -2,6 +2,8 @@
 def make_change(denomination_list, amount):
     '''Remove pass and implement the Greedy approach to make the change for the amount using the currencies in the denomination list.
     The function should return the total number of notes needed to make the change. If change cannot be obtained for the given amount, then return -1. Return 1 if the amount is equal to one of the currencies available in the denomination list.  '''
+    global coins
+    coins=[]
     denomination_list.sort(reverse=True)
     if amount in denomination_list:
         coins.append(amount)
@@ -22,6 +24,5 @@ def make_change(denomination_list, amount):
 #Pass different values to the function and test your program
 amount= 15
 denomination_list = [10,1,3]
-coins=[]
 print("no. of coins needed- ",make_change(denomination_list, amount))
 print("value of coins- ",coins)
