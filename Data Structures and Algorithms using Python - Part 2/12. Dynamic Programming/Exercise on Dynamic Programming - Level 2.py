1dd2cc75a84fb7c059cb413825775e4609cdf773
ASH <25776660+iash8090@users.noreply.github.com>
2023-03-13 18:18:05 +0530

    Update Exercise on Dynamic Programming - Level 2.py

--- Data Structures and Algorithms using Python - Part 2/12. Dynamic Programming/Exercise on Dynamic Programming - Level 2.py
+++ Data Structures and Algorithms using Python - Part 2/12. Dynamic Programming/Exercise on Dynamic Programming - Level 2.py
@@ -2,25 +2,14 @@
 def max_sum_is(num_list):
     #Remove pass and write your logic here
     n = len(num_list)
-    max = 0
-    msis = [0 for x in range(n)]
- 
-    for i in range(n):
-        msis[i] = num_list[i]
- 
+    new_list=num_list[:]
     for i in range(1, n):
         for j in range(i):
-            if (num_list[i] > num_list[j] and
-                msis[i] < msis[j] + num_list[i]):
-                msis[i] = msis[j] + num_list[i]
-    print(msis)
-
-    for i in range(n):
-        if max < msis[i]:
-            max = msis[i]
- 
-    return max
-
+            if (num_list[j] < num_list[i] and new_list[i] < new_list[j] + num_list[i]):
+                new_list[i] = new_list[j] + num_list[i]
+    print(new_list)
+    m=max(new_list) 
+    return m
 
 #Pass different values to the function and test your program
 num_list=[1, 101, 2, 3, 100, 4, 5]
